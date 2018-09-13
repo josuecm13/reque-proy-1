407e78e935f85a4c5f17bde543cd92e518d41bc1
Cthulhu-28 <smoyaqops@gmail.com>
2018-09-13 10:06:43 -0600

    Noticias y comentarios

--- Scripts SQL/Tablas.sql
+++ Scripts SQL/Tablas.sql
@@ -25,3 +25,22 @@ create table Band
 	banner_photo varbinary(max) null,
 	rating numeric(3,2) constraint band_rating_nn not null constraint band_rating_default default (5)
 )
+
+create table News
+(
+	author varchar(30) constraint new_username_fk references Account (username),
+	date datetime constraint news_date_nn not null,
+	title varchar(100) constraint news_title_nn not null,
+	body varchar(4000) constraint body_body_nn not null,
+	photo varbinary(max) null,
+	constraint news_pk primary key (author,date)
+
+)
+
+create table Comment
+(
+	author varchar(30),
+	date datetime,
+	constraint comment
+
+)
